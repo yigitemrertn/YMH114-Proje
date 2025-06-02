9df30cef67ab508cfdfedef6d2f8f972c09c2657
Yiğit Emre <yigitemreerten@gmail.com>
2025-06-02 06:24:22 +0300

    Post carda pp ekleme denemesi 5

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'u805253064_forumdb';
+$username = 'u805253064_yemauser';
+$password = 'YeMa6455';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?> 
